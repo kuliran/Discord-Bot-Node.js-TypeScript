ec9fe298c26575a295595e8e3d549683fea40ebb
kuliran <137825720+kuliran@users.noreply.github.com>
2024-08-31 22:39:55 +0300

    Delete dist directory

--- dist/systems/example/events/messageCreate/replyHi.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const botEvent = (client, message) => {
-    if (message.content !== "Hi")
-        return;
-    message.reply("Hi. Nice to see you");
-};
-exports.default = botEvent;
--- dist/systems/general/events/ready/readyLog.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const botEvent = (client) => {
-    console.log(`${client.user?.username} is online.`);
-};
-exports.default = botEvent;
